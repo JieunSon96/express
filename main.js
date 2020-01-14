3ca0408a0adee79115e02341ad7bb82317237dbe
JieunSon96 <plmn855000@gmail.com>
2020-01-14 17:57:47 +0900

    login process + authentication

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const bodyParser=require('body-parser');
 const cookieParser=require('cookie-parser');
 const config=require('./config/key');
 const {User} =require('./models/user');
+const {auth} =require('./middleware/auth');
 mongoose.connect(config.mongoURI
 ,{useNewUrlParser:true}).then(()=>console.log('DB Connected'))
                               .catch(err=>console.log(err));
@@ -22,17 +23,57 @@ app.get('/',(req,res)=>{
   })
 })
 
+app.get("/api/user/auth",auth,(req,res)=>{
+  res.status(200).json({
+    _id:req_id,
+    isAuth:true,
+    email:req.user.email,
+    name:req.user.name,
+    lastname:req.user.lastname,
+    role:req.user.role
+  })
+})
+
 app.post('/api/users/register',(req,res)=>{
   const user=new User(req.body)
 
-  user.save((err,userData)=>{
+  user.save((err,doc)=>{
     if(err) return res.json({success:false,err})
     return res.status(200).json({
-      success:true
+      success:true,
+      userData:doc
     })
   })
 
 });
+
+app.post('/api/user/login',(req,res)=>{
+  //find the email
+   User.findOne({email:req.body.email},(err,user)=>{
+     if(!user){
+       return res.json({
+         loginSuccess:false,
+         message:"Auth Failed, email Not Found"
+       });
+     }
+
+  //compare password
+  user.comparePassword(req.body.password,(err,isMatch) => {
+    if(!isMatch){
+      return res.json({loginSuccess:false,message:"wrong password!"})
+    }
+
+  })
+  //generateJWTToken
+  user.generateJWTToken((err,user)=>{
+    if(err) return res.status(400).send(err);
+    res.cookie("x-auth",user.token).status(200).json({
+      loginSuccess:true
+    })
+  })
+
+  });
+})
 
 
 // app.get('/',(req,res)=>{
